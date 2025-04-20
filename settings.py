09f2cd7428564f23aea53011e85752acdb15064c
MeOldNg <junweing1004@gmail.com>
2025-04-20 15:51:34 +0800

    Commit Godot Code into my branch

--- settings.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-class SettingsWindow(tk.Toplevel):
-    def __init__(self, root):
-        tk.Toplevel.__init__(self, root)
-
-        self.title("Settings")
-        self.resizable(False, False)
