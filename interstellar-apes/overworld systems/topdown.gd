b71f92912804be7bb3517b6610bbd60a7e987ec8
1221107067MMU <1221107067@student.mmu.edu.my>
2025-04-27 17:12:59 +0800

    completed interactable object + added animations for player

--- interstellar-apes/overworld systems/topdown.gd
+++ interstellar-apes/overworld systems/topdown.gd
@@ -8,13 +8,43 @@ var target_position = Vector2.ZERO
 var moving = false
 @onready var tile_map: TileMapLayer = $"../dungeon_tiles"
 @onready var ray_cast_2d = $RayCast2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var anim_dir
 var direction
 func _ready():
 	#starts the target at the current location
 	target_position = position
-
+	print(direction)
 
 func _physics_process(delta: float) -> void:
+	#animation attempt
+	if anim_dir == "left":
+		if moving:
+			animated_sprite_2d.play("walk_side")
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.play("idle_side")
+	if anim_dir == "right":
+		if moving:
+			animated_sprite_2d.play("walk_side")
+			animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.play("idle_side")
+	if anim_dir == "up":
+		if moving:
+			animated_sprite_2d.play("walk_up")
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.play("idle_up")
+	if anim_dir == "down":
+		if moving:
+			animated_sprite_2d.play("walk_down")
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.play("idle_down")
+
+	#preset interaction
 	if Input.is_action_just_pressed("ui_accept") and $RayCast2D.is_colliding():
 		var collider = $RayCast2D.get_collider()
 		collider.on_interact()
@@ -22,22 +52,24 @@ func _physics_process(delta: float) -> void:
 	if not moving:
 		direction = Vector2.ZERO
 		if Input.is_action_pressed("ui_up"):
+			anim_dir = "up"
 			move(Vector2.UP)
 		elif Input.is_action_pressed("ui_down"):
+			anim_dir = "down"
 			move(Vector2.DOWN)
 		elif Input.is_action_pressed("ui_left"):
+			anim_dir = "left"
 			move(Vector2.LEFT)
 		elif Input.is_action_pressed("ui_right"):
+			anim_dir = "right"
 			move(Vector2.RIGHT)
-
-
 	if moving:
 		position = position.move_toward(target_position, speed * delta)
 		if position.distance_to(target_position) < speed * delta:
 			position = target_position
-			moving = false	
+			moving = false
 			
-			
+
 func move(direction: Vector2):
 	
 	if direction != Vector2.ZERO:
@@ -50,16 +82,18 @@ func move(direction: Vector2):
 		#Get custom data layer from target tile
 		var tile_data: TileData = tile_map.get_cell_tile_data(target_tile)
 	
-		if tile_data.get_custom_data("walkable") == false and ray_cast_2d.is_colliding() == false:
-			print("fuck yeah")
+		if tile_data.get_custom_data("walkable") == false:
+			#print("is colliding")
 			return
-		else:
-			print("walkies")
+		
+		elif ray_cast_2d.is_colliding(): #checks if an entity is in the way
+			#print("somethings in the way")
+			return
+		else: #lets player walk around
+			#print("walkies")
 			target_position += direction * grid_size
 			moving = true
-			# the target position based on the direction and grid size
-
-		#if tile_map.get_custom_data("walkable") == true:
+	print(ray_cast_2d.target_position)
 
 #credits:
 #https://www.reddit.com/r/godot/comments/13qgcwc/hiw_to_make_grid_based_movement/
