5db628299970ee31a5901b09ce0d9c9d68b551e9
1221107067MMU <1221107067@student.mmu.edu.my>
2025-05-05 08:40:36 +0800

    still working on door

--- interstellar-apes/overworld systems/door.gd
+++ interstellar-apes/overworld systems/door.gd
@@ -1,20 +1,28 @@
 extends StaticBody2D
 
-var open = false
+var open = true
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var interact: CollisionShape2D = $collision_interact
+@onready var collision: CollisionShape2D = $CollisionShape2D
 
 
 
 func on_interact():
-	print(get_collision_layer_value(2))
 	open = !open
 	if open == false:
 		sprite_2d.modulate = Color("red")
-		set_collision_layer_value(2, true)
-		set_collision_mask_value(2, true)
-	else:
+		set_collision_layer_value(3, true)
+		print("CLOSED")
+		print("Collision layer 1 is: %s" % get_collision_layer_value(1))
+		print("Collision layer 2 is: %s" % get_collision_layer_value(2))
+		print("Collision layer 3 is: %s" % get_collision_layer_value(3))
+		print("Collision layer 4 is: %s" % get_collision_layer_value(4))
+	
+	if open == true:
 		sprite_2d.modulate = Color("green")
-		set_collision_layer_value(2, false)
-		set_collision_mask_value(2, false)
+		set_collision_layer_value(3, false)
+		print("OPENED")
+		print("Collision layer 1 is: %s" % get_collision_layer_value(1))
+		print("Collision layer 2 is: %s" % get_collision_layer_value(2))
+		print("Collision layer 3 is: %s" % get_collision_layer_value(3))
+		print("Collision layer 4 is: %s" % get_collision_layer_value(4))
