49c45340fd1c3e3a7d8aa14f22585a1592bb3002
MeOldNg <junweing1004@gmail.com>
2025-05-06 23:00:56 +0800

    Added background
    Added background back to the main menu but not in the settings menu yet

---
+++ main-menu/mainmenuscene/script/login_panel.gd
@@ -0,0 +1,12 @@
+extends Panel
+
+func _on_return_button_down() -> void:
+	queue_free()
+	pass # Replace with function body.
+
+func _on_signin_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_login_button_down() -> void:
+	pass # Replace with function body.
