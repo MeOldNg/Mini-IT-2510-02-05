786c0328d64db45d65315606834339341442ed88
MeOldNg <junweing1004@gmail.com>
2025-05-06 16:08:27 +0800

    Revert "Merge branch 'jw-branch'"
    This reverts commit 5f2625b72a0b142925f27b36b8fe5217066f0f38, reversing
    changes made to 75fefd774b6e452006a4f831d767044f5b94e604.

--- main-menu/settingsscene/ingame_settings.gd
+++ main-menu/settingsscene/ingame_settings.gd
@@ -9,8 +9,5 @@ func _on_graphics_button_pressed() -> void:
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main-menu/mainmenuscene/main-menu.tscn")
 	
-func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
