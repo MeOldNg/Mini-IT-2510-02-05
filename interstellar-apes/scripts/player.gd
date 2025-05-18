9add7c504e46e52cf26f6b1e78623f547af525db
MeOldNg <junweing1004@gmail.com>
2025-05-18 18:12:29 +0800

    Revert "Merge branch 'main-menu-branch' of https://github.com/MeOldNg/Mini-IT-2510-02-05 into main-menu-branch"
    This reverts commit 85671f07ab8d6663bb9f9f94f503acbf52487820, reversing
    changes made to 4e93221aa3317358078db226a95c9aa6f92b2920.

--- interstellar-apes/scripts/player.gd
+++ interstellar-apes/scripts/player.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
 
-@export var SPEED := 200
-@export var acceleration :=8.0
+@export var SPEED := 300
+@export var acceleration :=15.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var interactable: RayCast2D = $interactable
 
 var animation_direction: String = "down"
 var animation_state: String = ""
-
+var is_holding_something = false
 
 func update_sprite_direction(input: Vector2) -> void:
 	match input:
 		Vector2.DOWN:
 			animation_direction = "move_down"
+			if not is_holding_something:
+				interactable.target_position = Vector2.DOWN * 20
+				interactable.force_raycast_update()
 		Vector2.UP:
 			animation_direction = "move_up"
+			if not is_holding_something:
+				interactable.target_position = Vector2.UP * 20
+				interactable.force_raycast_update()
 		Vector2.RIGHT:
 			animation_direction = "move_right"
+			if not is_holding_something:
+				interactable.target_position = Vector2.RIGHT * 20
+				interactable.force_raycast_update()
 		Vector2.LEFT:
 			animation_direction = "move_left"
+			if not is_holding_something:
+				interactable.target_position = Vector2.LEFT * 20
+				interactable.force_raycast_update()
  
  
 
@@ -30,8 +43,23 @@ func update_sprite() -> void:
 		animation_state > "Idle"
  
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if $interactable.is_colliding():
+		var collider = $interactable.get_collider()
+		if collider.interact_type == "click":
+			if Input.is_action_just_pressed("ui_accept"):
+				collider.on_interact()
+		elif collider.interact_type == "hold":
+			if Input.is_action_just_pressed("ui_accept"):
+				is_holding_something =true
+				print("must be held")
+			elif Input.is_action_just_released("ui_accept"):
+				is_holding_something =false
+				print("I let go")
+		else:
+			print("somethings wrong")
 	
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
 	update_sprite_direction(direction)
 	update_sprite()
 	
@@ -42,3 +70,11 @@ func _physics_process(delta: float) -> void:
 	velocity.y = move_toward(velocity.y, direction.y*SPEED, acceleration)
 	
 	move_and_collide(velocity*delta)
+	
+
+	
+	
+	
+	
+	
+	
