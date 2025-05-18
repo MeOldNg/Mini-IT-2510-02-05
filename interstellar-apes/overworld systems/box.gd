dd36bb05f9811df505a46febdbead6c1d5d3d5dc
1221107067MMU <1221107067@student.mmu.edu.my>
2025-05-18 14:54:54 +0800

    save (just in case)

--- interstellar-apes/overworld systems/box.gd
+++ interstellar-apes/overworld systems/box.gd
@@ -5,7 +5,9 @@ var interact_type = "hold"
 var is_held = false
 func on_interact():
 	is_held = !is_held
-	while is_held == true:
+	if is_held == true:
 		global_position = player.global_position
 		print("is holding")
+	else:
+		pass
 	
