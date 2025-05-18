586444dbf93a91c10bc3f6c3d47d652362086970
MeOldNg <junweing1004@gmail.com>
2025-05-18 21:33:55 +0800

    small changes
    added ingame settings and remove graphics tab from settings as it is not needed anymore

--- main-menu/mainmenuscene/script/in-game_paused.gd
+++ main-menu/mainmenuscene/script/in-game_paused.gd
@@ -11,3 +11,6 @@ func _on_character_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main-menu/mainmenuscene/main-menu.tscn")
