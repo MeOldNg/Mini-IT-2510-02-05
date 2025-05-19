03a6f5c3c7fa654aaeb6239c75365fcecf614fa6
1221107067MMU <1221107067@student.mmu.edu.my>
2025-05-19 11:37:34 +0800

    reworked some shit

--- interstellar-apes/overworld systems/box.gd
+++ interstellar-apes/overworld systems/box.gd
@@ -1,13 +1,6 @@
-extends CharacterBody2D
+extends StaticBody2D
 @onready var player: CharacterBody2D = $"../CharacterBody2D"
 
 var interact_type = "hold"
-var is_held = false
-func on_interact():
-	is_held = !is_held
-	if is_held == true:
-		global_position = player.global_position
-		print("is holding")
-	else:
-		pass
+var box = true
 	
--- interstellar-apes/scripts/player.gd
+++ interstellar-apes/scripts/player.gd
@@ -1,75 +1,108 @@
 extends CharacterBody2D
 
 
-@export var SPEED := 300
+@export var SPEED := 150
 @export var acceleration :=15.0
-
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var interactable: RayCast2D = $interactable
 
 var animation_direction: String = "down"
-var animation_state: String = ""
+var animation_state: String = "Idle"
 var is_holding_something = false
 
-func update_sprite_direction(input: Vector2) -> void:
-	match input:
-		Vector2.DOWN:
-			animation_direction = "move_down"
-			if not is_holding_something:
-				interactable.target_position = Vector2.DOWN * 20
-				interactable.force_raycast_update()
-		Vector2.UP:
-			animation_direction = "move_up"
-			if not is_holding_something:
-				interactable.target_position = Vector2.UP * 20
-				interactable.force_raycast_update()
-		Vector2.RIGHT:
-			animation_direction = "move_right"
-			if not is_holding_something:
-				interactable.target_position = Vector2.RIGHT * 20
-				interactable.force_raycast_update()
-		Vector2.LEFT:
-			animation_direction = "move_left"
-			if not is_holding_something:
-				interactable.target_position = Vector2.LEFT * 20
-				interactable.force_raycast_update()
- 
- 
+#func update_sprite_direction(input: Vector2) -> void:
+	#match input:
+		#Vector2.DOWN:
+			#animation_direction = "move_down"
+			#if not is_holding_something:
+				#interactable.target_position = Vector2.DOWN * 20
+				#interactable.force_raycast_update()
+		#Vector2.UP:
+			#animation_direction = "move_up"
+			#if not is_holding_something:
+				#interactable.target_position = Vector2.UP * 20
+				#interactable.force_raycast_update()
+		#Vector2.RIGHT:
+			#animation_direction = "move_right"
+			#if not is_holding_something:
+				#interactable.target_position = Vector2.RIGHT * 20
+				#interactable.force_raycast_update()
+		#Vector2.LEFT:
+			#animation_direction = "move_left"
 
-func update_sprite() -> void:
-	if velocity.length() > 0:
-		animation_state > "move"
-	else:
-		animation_state > "Idle"
+ 
+ 
+#func update_sprite() -> void:
+	#if velocity != Vector2.ZERO:
+		#animation_state > "move"
+	#else:
+		#animation_state > "Idle"
+
  
 func _physics_process(delta: float) -> void:
-	if $interactable.is_colliding():
+	if $interactable.is_colliding(): # these are interactable codes
 		var collider = $interactable.get_collider()
 		if collider.interact_type == "click":
 			if Input.is_action_just_pressed("ui_accept"):
 				collider.on_interact()
 		elif collider.interact_type == "hold":
 			if Input.is_action_just_pressed("ui_accept"):
-				is_holding_something =true
-				print("must be held")
+				if collider.box:
+					is_holding_something =true
+					collider.global_position = interactable.target_position * interactable.global_transform
+					print("must be held")
 			elif Input.is_action_just_released("ui_accept"):
 				is_holding_something =false
 				print("I let go")
 		else:
 			print("somethings wrong")
-	
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	update_sprite_direction(direction)
-	update_sprite()
+
+	var input_vector = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		animation_direction = "up"
+		input_vector.y = -1
+		if not is_holding_something:
+			interactable.target_position = Vector2.UP * 20
+			interactable.force_raycast_update()
+	elif Input.is_action_pressed("move_down"):
+		animation_direction = "down"
+		input_vector.y = 1
+		if not is_holding_something:
+			interactable.target_position = Vector2.DOWN * 20
+			interactable.force_raycast_update()
+	elif Input.is_action_pressed("move_left"):
+		input_vector.x = -1
+		animation_direction = "left"
+		if not is_holding_something:
+			interactable.target_position = Vector2.LEFT * 20
+			interactable.force_raycast_update()
+	elif Input.is_action_pressed("move_right"):
+		animation_direction = "right"
+		input_vector.x = 1
+		if not is_holding_something:
+			interactable.target_position = Vector2.RIGHT * 20
+			interactable.force_raycast_update()
+	else:
+		velocity = Vector2.ZERO
+	velocity = input_vector * SPEED
+	move_and_slide()
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if velocity != Vector2.ZERO:
+		animation_state = "move"
+	else:
+		animation_state = "Idle"
 	
 	
+	#update_sprite_direction(direction)
+	#update_sprite()		
 	animated_sprite_2d.play(animation_state+animation_direction)
-	
-	velocity.x = move_toward(velocity.x, direction.x*SPEED, acceleration)
-	velocity.y = move_toward(velocity.y, direction.y*SPEED, acceleration)
-	
-	move_and_collide(velocity*delta)
+	#
+	#velocity.x = move_toward(velocity.x, direction.x*SPEED, acceleration)
+	#velocity.y = move_toward(velocity.y, direction.y*SPEED, acceleration)
+	#
+	#move_and_collide(velocity*delta)
 	
 
 	
