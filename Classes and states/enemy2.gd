1db177047b88517eb898775b5f4bd6c70262219c
1221107067MMU <1221107067@student.mmu.edu.my>
2025-05-26 09:05:28 +0800

    enemy patrol mechanic completed (I can finally grill myself)

--- Classes and states/enemy2.gd
+++ Classes and states/enemy2.gd
@@ -1,35 +1,43 @@
 extends CharacterBody2D
-class_name enemy
-const speed = 100
 
+enum states {patrol, alert, chase}
+
+var speed = 150
+var state: states = states.patrol
 @export var group_name : String
-var target 
-
+@export var target : Node = null
 
 var positions : Array #positions for wandering
 var temp_posititions : Array #temporary var to store positions
 var current_position : Marker2D #current position
 
-
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	print(self.global_position)
 	var vector = Vector2.ZERO
 	if position.y - target.position.y < -1:
 		vector.y += 1
 	elif position.y - target.position.y > 1:
 		vector.y -= 1
-	elif position.x - target.position.x < -1:
+	elif position.x - target.position.x < 1:
 		vector.x += 1
 	elif position.x - target.position.x > 1:
 		vector.x -= 1
 	velocity = vector * speed
 	move_and_slide()
-	#print(target.position)
+	if state == states.patrol:
+		if global_position.distance_to(target.position) < 10:
+			get_next_position()
+			
+func change_target(received_target):
+	target = received_target
 
-func ready():
+
+func _ready() -> void:
+	$NavigationAgent2D.target_position = target.position
 	positions = get_tree().get_nodes_in_group(group_name)
 	get_positions()
 	get_next_position()
-
+	
 func get_positions():
 	temp_posititions= positions.duplicate()
 	
@@ -38,6 +46,4 @@ func get_next_position():
 		get_positions()
 	current_position = temp_posititions.pop_front()
 	change_target(current_position)
-
-func change_target(received_target):
-	target = received_target
+	print (current_position.position)
--- interstellar-apes/overworld systems/enemyidle.gd
+++ interstellar-apes/overworld systems/enemyidle.gd
@@ -2,7 +2,7 @@ extends State
 class_name  enemy_idle
 
 @export var group_name : String
-@onready var enemy: enemy = $".."
+@onready var enemy = $".."
 
 var speed = 150
 var positions : Array #positions for wandering
@@ -19,17 +19,6 @@ func exit():
 	pass
 
 func update(_delta:float):
-	var vector = Vector2.ZERO
-
-	if enemy.position.y - current_position.y < -1:
-		vector.y += 1
-	elif enemy.position.y - current_position.y > 1:
-		vector.y -= 1
-	elif enemy.position.x - current_position.position.x < -1:
-		vector.x += 1
-	elif enemy.position.x - current_position.position.x > 1:
-		vector.x -= 1
-	enemy.velocity = vector * speed
 	if enemy.global_position.distance_to(current_position.position) < 10:
 		get_next_position()
 
@@ -42,4 +31,5 @@ func get_next_position():
 	if temp_posititions.is_empty():
 		get_positions()
 	current_position = temp_posititions.pop_front()
+	enemy.target_received(current_position)
 	
